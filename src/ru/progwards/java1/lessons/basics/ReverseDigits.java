b6849da979cde06a04c5917aa75ea68fe6a050ac
Alex <rostovpapa@mail.ru>
2019-12-10 17:02:56 +0300

    Start

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -2,7 +2,7 @@ package ru.progwards.java1.lessons.basics;
 
 public class ReverseDigits {
     public static void main(String[] args) {
-        System.out.println(reverseDigits(491));
+        reverseDigits(123);
     }
 
     public static int reverseDigits(int number) {
