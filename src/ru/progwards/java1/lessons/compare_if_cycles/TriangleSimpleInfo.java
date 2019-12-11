6fdeaff53f35cfd2946d4ca8b47a94439c4402e3
Alex <rostovpapa@mail.ru>
2019-12-11 16:23:44 +0300

    Start

---
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -0,0 +1,35 @@
+package ru.progwards.java1.lessons.compare_if_cycles;
+
+public class TriangleSimpleInfo {
+    public static void main(String[] args) {
+        maxSide(5, 8, 9);
+        minSide(2, 1, 0);
+        isEquilateralTriangle(2, 2, 2);
+        isEquilateralTriangle(1, 2, 1);
+    }
+
+    public static int maxSide(int a, int b, int c) {
+        if (b < a && c < a) {
+            return a;
+        } else if (a < b && c < b) {
+            return b;
+        } else
+            return c;
+    }
+
+    public static int minSide(int a, int b, int c) {
+        if (b > a && c > a) {
+            return a;
+        } else if (a > b && c > b) {
+            return b;
+        } else
+            return c;
+    }
+
+    public static boolean isEquilateralTriangle(int a, int b, int c) {
+        return (a == b && b == c);
+    }
+}
+
+
+
