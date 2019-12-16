461d29978ab528e1c7b91a7582751aec2489ee91
Alex <rostovpapa@mail.ru>
2019-12-16 08:49:24 +0300

    Start

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -9,21 +9,23 @@ public class CyclesGoldenFibo {
 
         for (int i = 1; i <= 100; i++) {
             for (int j = 1; j < 100; j++) {
-
                 if (isGoldenTriangle(i, j, j)) {
                     System.out.println(j + " " + i + " " + i);
                 }
             }
         }
-        containsDigit(123, 2);
+        System.out.println(containsDigit(0, 0));
         fiboNumber(10);
         isGoldenTriangle(0, 0, 0);
     }
 
     public static boolean containsDigit(int number, int digit) {
         int result = 0;
+        if (number == 0 && digit == 0) {
+            return true;
+        }
         number = number * 10;
-        for (int i = 0; ((number /= 10) != 0); i++) {
+        while ((number /= 10) != 0) {
             result = number % 10;
             if (result == digit) {
                 return true;
@@ -56,17 +58,14 @@ public class CyclesGoldenFibo {
         if (a == 0 || b == 0 || c == 0) {
             return false;
         }
-        if ((a == b) && (1.61703 < (float) a / (float) c && (float) a / (float) c < 1.61903)) {
+        if ((a == b) && (1.61703 <= (float) a / (float) c && (float) a / (float) c <= 1.61903)) {
             return true;
         }
-        if ((a == c) && (1.61703 < (float) b / (float) c && (float) b / (float) c < 1.61903)) {
+        if ((a == c) && (1.61703 <= (float) b / (float) c && (float) b / (float) c <= 1.61903)) {
             return true;
         }
-        if ((b == c) && (1.61703 < (float) c / (float) a && (float) c / (float) a < 1.61903)) {
+        if ((b == c) && (1.61703 <= (float) c / (float) a && (float) c / (float) a <= 1.61903)) {
             return true;
         } else return false;
     }
 }
-
-
-
