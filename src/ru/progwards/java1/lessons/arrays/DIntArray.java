9b444d3b4ded6b373fa61a73610ef3f7c664e9f5
Alex <rostovpapa@mail.ru>
2020-01-12 15:50:15 +0300

    Start

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -12,7 +12,7 @@ public class DIntArray {
     public void add(int num) {
         int[] massNew = Arrays.copyOf(mass, mass.length + 1);
         massNew[massNew.length - 1] = num;
-        mass = Arrays.copyOf(mass, mass.length + 1);
+        mass = Arrays.copyOf(massNew, massNew.length);
     }
 
     public void atInsert(int pos, int num) {
@@ -20,13 +20,13 @@ public class DIntArray {
         System.arraycopy(mass, 0, massNew, 0, massNew.length);
         massNew[pos] = num;
         System.arraycopy(mass, pos, massNew, pos + 1, massNew.length);
-        mass = Arrays.copyOf(mass, mass.length + 1);
+        mass = Arrays.copyOf(massNew, massNew.length);
     }
 
     public void atDelete(int pos) {
         int[] massNew = new int[mass.length - 1];
         System.arraycopy(mass, pos, massNew, pos - 1, massNew.length);
-        mass = Arrays.copyOf(mass, mass.length + 1);
+        mass = Arrays.copyOf(massNew, massNew.length);
     }
 
     public int at(int pos) {
