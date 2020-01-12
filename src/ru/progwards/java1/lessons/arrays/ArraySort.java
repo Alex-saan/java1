04debfd7e6c24dca72b9e7a8ad9bb1f8304c58b7
Alex <rostovpapa@mail.ru>
2020-01-12 13:18:06 +0300

    Start

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class ArraySort {
     public static void main(String[] args) {
-        int[] a = {1, 9, 3, 1, 8, 2};
+        int[] a = {1, 9, 3, 1, 8, 2, 5, -7, 5, 77, 11, -3};
         sort(a);
     }
 
     public static void sort(int[] a) {
-        for (int i = 1; i < a.length; ++i) {
+        for (int i = 0; i < a.length; ++i) {
             for (int j = i + 1; j < a.length; ++j) {
                 if (a[j] < a[i]) {
                     int tmp = a[i];
