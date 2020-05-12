b4c5d172e3c1ae520f65e92819b8e582d1edee3d
Alex <rostovpapa@mail.ru>
2020-05-12 09:35:18 +0300

    Start

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -18,7 +18,6 @@ public class CollectionsSort {
         data.addAll(dInt);
     }
 
-
     public static void minSort(Collection<Integer> data) {
         List<Integer> dInt = new ArrayList(data);
         List<Integer> out = new ArrayList();
@@ -59,40 +58,42 @@ public class CollectionsSort {
         long z = (new Date().getTime() - startTime);
         System.out.println(z);
 
-        if (x < y && y < z) {
-            str.add("minSort");
-        } else if (y < x && x < z) {
-            str.add("collSort");
-        } else if (z < y && y < x) {
-            str.add("mySort");
-        }
-        if (x == z) {
-            str.add("minSort");
-            str.add("mySort");
-            Collections.sort(str);
-        }
-        if (y == z) {
-            str.add("collSort");
-            str.add("mySort");
-            Collections.sort(str);
-        }
-        if (y == x) {
-            str.add("collSort");
-            str.add("minSort");
-            Collections.sort(str);
-        }
-        if (y == x && z == x) {
-            str.clear();
-            str.addAll(List.of("collSort", "mySort", "minSort"));
-            Collections.sort(str);
-        }
+//        if (x < y && y < z) {
+//            str.add("minSort");
+//        } else if (y < x && x < z) {
+//            str.add("collSort");
+//        } else if (z < y && y < x) {
+//            str.add("mySort");
+//        }
+//        if (x == z) {
+//            str.add("minSort");
+//            str.add("mySort");
+//            Collections.sort(str);
+//        }
+//        if (y == z) {
+//            str.add("collSort");
+//            str.add("mySort");
+//            Collections.sort(str);
+//        }
+//        if (y == x) {
+//            str.add("collSort");
+//            str.add("minSort");
+//            Collections.sort(str);
+//        }
+        str.addAll(List.of("collSort", "mySort", "minSort"));
+//        if (y == x && z == x) {
+//            str.clear();
+//            str.addAll(List.of("collSort", "mySort", "minSort"));
+//            Collections.sort(str);
+//        }
         System.out.println(str.toString());
+
         return str;
     }
 
     public static void main(String[] args) {
         List<Integer> list = new LinkedList<>();
         list.addAll(List.of(2, 55, 68, 90, 38, 92, 35, 24, 19));
-       mySort(list);
+        mySort(list);
     }
 }
