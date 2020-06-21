94818587d94677b8b67b44be9043f1eff8c0e3d4
Alex-saan <rostovpapa@mail.ru>
2020-06-21 13:28:33 +0300

    Start

---
+++ src/ru/progwards/java1/lessons/files/FilesSelect.java
@@ -0,0 +1,60 @@
+package ru.progwards.java1.lessons.files;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilesSelect {
+    String file = "";
+
+    public void selectFiles(String inFolder, String outFolder, List<String> keys) {
+        Path pathIn = Paths.get("d:/inFolder");
+        Path pathOut = Paths.get("d:/outFolder");
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*.txt");
+
+        try {
+            Files.walkFileTree(Paths.get(inFolder), new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                    if (pathMatcher.matches(path)) {
+                        try {
+                            file = Files.readString(path);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        for (String key : keys) {
+                            if (file.contains(key)) {
+                                String fileInKey = key;
+                                Path newDir = path.resolve(fileInKey);
+                                try {
+                                    Files.createDirectory(newDir);
+                                    Files.copy(pathIn, pathOut, StandardCopyOption.REPLACE_EXISTING);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                    System.out.println(path);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException e) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        FilesSelect filesSelect = new FilesSelect();
+        List<String> list = new ArrayList<>();
+        list.add("string");
+        filesSelect.selectFiles("d:/inFolder", "d:/outFolder", list);
+    }
+}
---
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -0,0 +1,67 @@
+package ru.progwards.java1.lessons.files;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindDuplicates {
+    public List<List<String>> findDuplicates(String startPath) {
+        List<Path> list = new ArrayList<>();
+        List<List<String>> listOut = new ArrayList<>();
+        try {
+            Files.walkFileTree(Paths.get(startPath), new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                    list.add(path);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException e) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            Path pathI = list.get(i);
+            for (int j = 1; j < list.size(); j++) {
+                Path pathJ = list.get(j);
+                Path pI = pathI.getFileName();
+                Path pJ = pathJ.getFileName();
+                if (pI.equals(pJ)) {
+                    try {
+                        if (
+                                (Files.readAttributes(pathI, "lastModifiedTime").equals(
+                                        Files.readAttributes(pathJ, "lastModifiedTime")) &&
+                                        Files.size(pathI) == Files.size(pathJ)
+                                )) {
+                            if (Arrays.equals(Files.readAllBytes(pathI),
+                                    Files.readAllBytes(pathJ))) {
+                                List<String> listN = new ArrayList<>();
+                                listN.add(String.valueOf(pathI));
+                                listN.add(String.valueOf(pathJ));
+                                listOut.add(listN);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        System.out.println(listOut);
+        return listOut;
+    }
+
+    public static void main(String[] args) {
+        FindDuplicates findDuplicates = new FindDuplicates();
+        findDuplicates.findDuplicates("C:\\Users\\user\\Desktop\\Java");
+    }
+}
